f387146074277c04d6ea5d74144b8d0ee2e719fc
pauloantonelli <pauloantonelli@hotmail.com>
2018-12-06 12:13:57 -0200

    fim da aula

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro-componente',
+  template: `<p>Meu primeiro componente em Angular 7!</p>`,
+})
+
+export class MeuPrimeiroComponent {}
